ed7423c4dfb11aaae7467aa6215a841ac20c534e
faisalmoinuddin99 <faisal25march99@gmail.com>
2024-02-28 15:33:21 +0530

    Running sum of 1 D Array

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -9,8 +9,21 @@ def print_hi(name):
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
 
 
+def swapping_without_emp(a, b):
+    x, y = b, a
+
+    return x, y
+
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Running sum of 1D Array
+    nums = [3, 1, 2, 10, 1]
+    # expected output [3, 4, 6, 16, 17]
+    op = []
+    for i in range(1, len(nums)):
+        nums[i] = nums[i-1] + nums[i]
+    print(nums) # [3, 4, 6, 16, 17]
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
